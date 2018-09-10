7b248ad3fb8525d6b0ec5d80b1f5ae1f188bb1b1
JustineAyroor <justinemessi@yahoo.in>
2018-09-10 00:34:19 +0000

    update dbURL code to adapt with resp env

--- app.js
+++ app.js
@@ -18,10 +18,9 @@ var commentRoutes    = require("./routes/comment"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-console.log(process.env.DATABASEURL);
-mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
-//mongoose.connect("mongodb://localhost:27017/yelp_camp_v13", { useNewUrlParser: true });
-// mongoose.connect("mongodb://Justine:11choco10@ds251362.mlab.com:51362/yelpcamp", { useNewUrlParser: true });
+var url = process.env.DATABASEURL || "mongodb://localhost:27017/yelp_camp_v13";
+//console.log(url);
+mongoose.connect(url, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
